51d58924f39bc3e42f2e3f734e23194c42b85501
James Wheadon <james.wheadon@yahoo.com>
2021-03-05 12:57:05 +0000

    fix issues, update paths

--- server/data.js
+++
@@ -1,7 +0,0 @@
-const meals = [
-    { id: 1, name: 'Lasagna', price: 5.00 },
-    { id: 2, name: 'Cheeseburger', price: 7.00 },
-    { id: 3, name: "Kebab", price: 6.00 },
-];
-
-module.exports = cats;
