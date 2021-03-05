db28ee23b877b9fddd6ae1e42fdcf73294a9c7a5
James Wheadon <james.wheadon@yahoo.com>
2021-03-05 12:24:13 +0000

    add server.js to server folder

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const mealRoutes = require('./controllers/meals');
+app.use('/meals', mealRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello there!');
+});
+
+app.post('/', (req, res) => {
+    res.status(405).send('Not allowd!');
+});
+
+module.exports = app;
