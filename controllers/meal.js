1bbc98ed4edcfb9d09d54af9a922da5381de7fb4
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-03-08 15:50:20 +0000

    added update functionality

--- controllers/meal.js
+++ controllers/meal.js
@@ -1,8 +1,12 @@
 const express = require('express');
+// const bodyParser = require('body-parser');
 const router = express.Router();
+
+// router.use(bodyParser.json);
 
 const meal = require('../models/meal');
 
+// READ
 router.get('/', (req, res) => {
     const mealData = meal.all;
     res.send(mealData);
@@ -19,17 +23,38 @@ router.get('/:id', (req, res) => {
     }
 });
 
+// CREATE
 router.post('/', (req, res) => {
     const data = req.body;
     const newMeal = meal.create(data);
     res.status(201).send(newMeal);
 });
 
+// DELETE
 router.delete('/:id', (req, res) => {
     const mealId = parseInt(req.params.id);
     const mealDelete = meal.findById(mealId);
     mealDelete.destroy();
-    res.status(204).send();
+    res.status(204).send(mealDelete);
 });
+
+// UPDATE
+router.put('/:id', (req, res) => {
+    const mealId = parseInt(req.params.id);
+    const mealUpdate = meal.findById(mealId);
+    const update = req.body;
+    console.log(update);
+    mealUpdate.update(update);
+    res.send(mealUpdate);
+})
+
+router.patch('/:id', (req, res) => {
+    const mealId = parseInt(req.params.id);
+    const mealUpdate = meal.findById(mealId);
+    const update = req.body;
+    console.log(update);
+    mealUpdate.update(update);
+    res.send(mealUpdate);
+})
 
 module.exports = router;
